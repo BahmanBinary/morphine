14ba2b0558a03c59b3446e56a14a8c7792de5e7d
Bahman Binary <bahman.binary@gmail.com>
2022-08-01 23:53:51 +0430

    add button controls and documents

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -3,6 +3,7 @@ import "./styles.css";
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
+    expanded: true,
     matchers: {
       color: /(background|color)$/i,
       date: /Date$/,
